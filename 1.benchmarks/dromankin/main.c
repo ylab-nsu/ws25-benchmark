75255e56d742c203e0672a80a71fb82fb8eda9d7
dromankin <d.romankin@g.nsu.ru>
2025-02-04 17:44:18 +0700

    Add volatile annotation

--- 1.benchmarks/dromankin/main.c
+++ 1.benchmarks/dromankin/main.c
@@ -1,67 +1,64 @@
 
 #include <inttypes.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-
-
-
-
-double fmadd (volatile double d, double a, double b, double c) {
-	int32_t m = 100000000;
-	for (int32_t i = 0; i < m; i++) {
-		d = a + b * c;
-		d = a + b * c;
-		d = a + b * c;
-		d = a + b * c;
-		d = a + b * c;
-		d = a + b * c;
-		d = a + b * c;
-		d = a + b * c;
-		d = a + b * c;
-		d = a + b * c;
-	}
-	return d;
+double fmadd(volatile double d, double a, double b, double c) {
+    int32_t m = 100000000;
+    for (int32_t i = 0; i < m; i++) {
+        d = a + b * c;
+        d = a + b * c;
+        d = a + b * c;
+        d = a + b * c;
+        d = a + b * c;
+        d = a + b * c;
+        d = a + b * c;
+        d = a + b * c;
+        d = a + b * c;
+        d = a + b * c;
+    }
+    return d;
 }
 
-double fmsub (double d, double a, double b, double c) {
-	int32_t m = 1000000000;
-	for (int32_t i = 0; i < m; i++) {
-		d = a - b * c;
-		d = a - b * c;
-		d = a - b * c;
-		d = a - b * c;
-		d = a - b * c;
-		d = a - b * c;
-		d = a - b * c;
-		d = a - b * c;
-		d = a - b * c;
-		d = a - b * c;
-
-	}
-	return d;
+double fmsub(volatile double d, double a, double b, double c) {
+    int32_t m = 1000000000;
+    for (int32_t i = 0; i < m; i++) {
+        d = a - b * c;
+        d = a - b * c;
+        d = a - b * c;
+        d = a - b * c;
+        d = a - b * c;
+        d = a - b * c;
+        d = a - b * c;
+        d = a - b * c;
+        d = a - b * c;
+        d = a - b * c;
+    }
+    return d;
 }
-
-
 
 int main(int argc, char** argv) {
-	if (argc != 2) {
-		printf("usage: main 1-fmadd; 2 = fmsub\n");
-		exit(1);
-	}
-	
-	int32_t arg = 0;
-	arg = atoi(argv[1]);
-	double a, b, c, d;
-	a = 4.0f;
-	b = 1.5f;
-	c = 2.4f;
-	d = 8.999f;
+    if (argc != 2) {
+        printf("usage: main 1-fmadd; 2 = fmsub\n");
+        exit(1);
+    }
 
-	switch(arg) {
-		case 1: a = fmadd(a, b, c); break;
-		case 2: a = fmsub(a, b, c); break;
-	}
-	
-	return 0;
+    int32_t arg = 0;
+    arg = atoi(argv[1]);
+    volatile double a, b, c, d;
+    a = 4.0f;
+    b = 1.5f;
+    c = 2.4f;
+    d = 8.999f;
+
+    switch (arg) {
+        case 1:
+            a = fmadd(d, a, b, c);
+            break;
+        case 2:
+            a = fmsub(d, a, b, c);
+            break;
+    }
+
+    return 0;
 }
--- 1.benchmarks/dromankin/main_asm.c
+++ 1.benchmarks/dromankin/main_asm.c
@@ -3,134 +3,97 @@
 #include <stdlib.h>
 
 double fmadd(volatile double d, double a, double b, double c) {
-	int32_t m = 100000000;
-	for (int32_t i = 0; i < m; i++) {
-	asm volatile(
-		"fmadd.d %0, %1, %2, %3"
-		: "=f"(d)
-		: "f"(b), "f"(c)  , "f"(a)
-	);
-	asm volatile(
-		"fmadd.d %0, %1, %2, %3"
-		: "=f"(d)
-		: "f"(b), "f"(c)  , "f"(a)
-	);
-	asm volatile(
-		"fmadd.d %0, %1, %2, %3"
-		: "=f"(d)
-		: "f"(b), "f"(c)  , "f"(a)
-	);
-	asm volatile(
-		"fmadd.d %0, %1, %2, %3"
-		: "=f"(d)
-		: "f"(b), "f"(c)  , "f"(a)
-	);
-	asm volatile(
-		"fmadd.d %0, %1, %2, %3"
-		: "=f"(d)
-		: "f"(b), "f"(c)  , "f"(a)
-	);
-	asm volatile(
-		"fmadd.d %0, %1, %2, %3"
-		: "=f"(d)
-		: "f"(b), "f"(c)  , "f"(a)
-	);
-	asm volatile(
-		"fmadd.d %0, %1, %2, %3"
-		: "=f"(d)
-		: "f"(b), "f"(c)  , "f"(a)
-	);
-	asm volatile(
-		"fmadd.d %0, %1, %2, %3"
-		: "=f"(d)
-		: "f"(b), "f"(c)  , "f"(a)
-	);
-	asm volatile(
-		"fmadd.d %0, %1, %2, %3"
-		: "=f"(d)
-		: "f"(b), "f"(c)  , "f"(a)
-	);
-	asm volatile(
-		"fmadd.d %0, %1, %2, %3"
-		: "=f"(d)
-		: "f"(b), "f"(c)  , "f"(a)
-	);
-	}
-	return d;
+    int32_t m = 100000000;
+    for (int32_t i = 0; i < m; i++) {
+        asm volatile("fmadd.d %0, %1, %2, %3"
+                     : "=f"(d)
+                     : "f"(b), "f"(c), "f"(a));
+        asm volatile("fmadd.d %0, %1, %2, %3"
+                     : "=f"(d)
+                     : "f"(b), "f"(c), "f"(a));
+        asm volatile("fmadd.d %0, %1, %2, %3"
+                     : "=f"(d)
+                     : "f"(b), "f"(c), "f"(a));
+        asm volatile("fmadd.d %0, %1, %2, %3"
+                     : "=f"(d)
+                     : "f"(b), "f"(c), "f"(a));
+        asm volatile("fmadd.d %0, %1, %2, %3"
+                     : "=f"(d)
+                     : "f"(b), "f"(c), "f"(a));
+        asm volatile("fmadd.d %0, %1, %2, %3"
+                     : "=f"(d)
+                     : "f"(b), "f"(c), "f"(a));
+        asm volatile("fmadd.d %0, %1, %2, %3"
+                     : "=f"(d)
+                     : "f"(b), "f"(c), "f"(a));
+        asm volatile("fmadd.d %0, %1, %2, %3"
+                     : "=f"(d)
+                     : "f"(b), "f"(c), "f"(a));
+        asm volatile("fmadd.d %0, %1, %2, %3"
+                     : "=f"(d)
+                     : "f"(b), "f"(c), "f"(a));
+        asm volatile("fmadd.d %0, %1, %2, %3"
+                     : "=f"(d)
+                     : "f"(b), "f"(c), "f"(a));
+    }
+    return d;
 }
 
-
 double fmsub(volatile double d, double a, double b, double c) {
-	int32_t m = 100000000;
-	for (int32_t i = 0; i < m; i++) {
-	asm volatile(
-		"fmsub.d %0, %1, %2, %3"
-		: "=f"(d)
-		: "f"(b), "f"(c)  , "f"(a)
-	);
-	asm volatile(
-		"fmsub.d %0, %1, %2, %3"
-		: "=f"(d)
-		: "f"(b), "f"(c)  , "f"(a)
-	);
-	asm volatile(
-		"fmsub.d %0, %1, %2, %3"
-		: "=f"(d)
-		: "f"(b), "f"(c)  , "f"(a)
-	);
-	asm volatile(
-		"fmsub.d %0, %1, %2, %3"
-		: "=f"(d)
-		: "f"(b), "f"(c)  , "f"(a)
-	);
-	asm volatile(
-		"fmsub.d %0, %1, %2, %3"
-		: "=f"(d)
-		: "f"(b), "f"(c)  , "f"(a)
-	);
-	asm volatile(
-		"fmsub.d %0, %1, %2, %3"
-		: "=f"(d)
-		: "f"(b), "f"(c)  , "f"(a)
-	);
-	asm volatile(
-		"fmsub.d %0, %1, %2, %3"
-		: "=f"(d)
-		: "f"(b), "f"(c)  , "f"(a)
-	);
-	asm volatile(
-		"fmsub.d %0, %1, %2, %3"
-		: "=f"(d)
-		: "f"(b), "f"(c)  , "f"(a)
-	);
-	asm volatile(
-		"fmsub.d %0, %1, %2, %3"
-		: "=f"(d)
-		: "f"(b), "f"(c)  , "f"(a)
-	);
-	asm volatile(
-		"fmsub.d %0, %1, %2, %3"
-		: "=f"(d)
-		: "f"(b), "f"(c)  , "f"(a)
-	);
-	}
-	return d;
+    int32_t m = 100000000;
+    for (int32_t i = 0; i < m; i++) {
+        asm volatile("fmsub.d %0, %1, %2, %3"
+                     : "=f"(d)
+                     : "f"(b), "f"(c), "f"(a));
+        asm volatile("fmsub.d %0, %1, %2, %3"
+                     : "=f"(d)
+                     : "f"(b), "f"(c), "f"(a));
+        asm volatile("fmsub.d %0, %1, %2, %3"
+                     : "=f"(d)
+                     : "f"(b), "f"(c), "f"(a));
+        asm volatile("fmsub.d %0, %1, %2, %3"
+                     : "=f"(d)
+                     : "f"(b), "f"(c), "f"(a));
+        asm volatile("fmsub.d %0, %1, %2, %3"
+                     : "=f"(d)
+                     : "f"(b), "f"(c), "f"(a));
+        asm volatile("fmsub.d %0, %1, %2, %3"
+                     : "=f"(d)
+                     : "f"(b), "f"(c), "f"(a));
+        asm volatile("fmsub.d %0, %1, %2, %3"
+                     : "=f"(d)
+                     : "f"(b), "f"(c), "f"(a));
+        asm volatile("fmsub.d %0, %1, %2, %3"
+                     : "=f"(d)
+                     : "f"(b), "f"(c), "f"(a));
+        asm volatile("fmsub.d %0, %1, %2, %3"
+                     : "=f"(d)
+                     : "f"(b), "f"(c), "f"(a));
+        asm volatile("fmsub.d %0, %1, %2, %3"
+                     : "=f"(d)
+                     : "f"(b), "f"(c), "f"(a));
+    }
+    return d;
 }
 
 int main(int argc, char** argv) {
-	if (argc != 2) {
-		printf("usage: ./main_asm 1 - fmadd; 2 - fmsub\n");
-		exit(1);
-	}
-	int32_t arg = 0;
-	arg = atoi(argv[1]);
-	double a = 3.0f;
-	double b = 2.0f;
-	double c = 4.0f;
-	volatile double d = 0.0f;
-	switch(arg) {
-		case 1: d = fmadd(d, a, b, c); break;
-		case 2: d = fmsub(d, a, b, c); break;
-	}
-	return 0;
+    if (argc != 2) {
+        printf("usage: ./main_asm 1 - fmadd; 2 - fmsub\n");
+        exit(1);
+    }
+    int32_t arg = 0;
+    arg = atoi(argv[1]);
+    double a = 3.0f;
+    double b = 2.0f;
+    double c = 4.0f;
+    volatile double d = 0.0f;
+    switch (arg) {
+        case 1:
+            d = fmadd(d, a, b, c);
+            break;
+        case 2:
+            d = fmsub(d, a, b, c);
+            break;
+    }
+    return 0;
 }
