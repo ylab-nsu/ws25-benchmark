341011aefe58c09cbfded0237724ea4e2fc0bb51
dromankin <d.romankin@g.nsu.ru>
2025-02-06 23:53:36 +0700

    Add new scripts with repeats (needs to be tested)

---
+++ 1.benchmarks/dromankin/scripts/parse.sh
@@ -0,0 +1,19 @@
+rep=20
+iter=1000
+n=100000
+gcc -g main.c -o main -O0 -pg
+if [[ $1 == 1 ]]; then
+	for ((i = 1; i < $n + 1; i+=$iter))
+	do
+	perf stat -o out_fmadd.txt -r $rep --table ./main $1 $i
+	grep ") #" out_fmadd.txt > out2_fmadd.txt
+	cat out2_fmadd.txt | cut -d"(" -f1 >> out3_fmadd.txt
+	done
+else
+	for ((i = 1; i < $n + 1; i+=$iter))
+	do
+	perf stat -o out_fmsub.txt -r $rep --table ./main $1 $i
+	grep ") #" out_fmsub.txt > out2_fmsub.txt
+	cat out2_fmsub.txt | cut -d"(" -f1 >> out3_fmsub.txt
+	done
+fi
