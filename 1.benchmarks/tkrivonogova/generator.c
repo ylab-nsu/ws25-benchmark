ff1b3143b960dd39b9e51c7d71a66b32c69c4744
tatyanakrivonogova <t.krivonogova@g.nsu.ru>
2025-02-07 15:50:45 +0700

    formatting

--- 1.benchmarks/tkrivonogova/generator.c
+++ 1.benchmarks/tkrivonogova/generator.c
@@ -1,7 +1,7 @@
+#include <inttypes.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <inttypes.h>
 
 #define ARRAY_SIZE 1000000
 
@@ -15,7 +15,8 @@ void generate_random_numbers(const char* filename) {
     srand(42);  // Фиксированный сид для генерации случайных чисел
 
     for (uint32_t i = 0; i < ARRAY_SIZE; i++) {
-        uint32_t random_number = rand() % 100000;  // Генерация числа от 0 до 99999
+        uint32_t random_number =
+            rand() % 100000;  // Генерация числа от 0 до 99999
         fprintf(file, "%u\n", random_number);
     }
 
